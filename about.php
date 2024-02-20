a8d3c3523d44e508522c83d1e19f10a78c14697a
Samitha Shiwankara <samithashiwankara46@gmail.com>
2024-02-20 10:13:04 +0530

    initial commit

---
+++ about.php
@@ -0,0 +1,143 @@
+<!DOCTYPE HTML>
+
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+	<link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
+	<script src="js/jquery.min.js"></script>
+</head>
+
+<body>
+	<div class="header">
+		<div class="header_top">
+			<div class="wrap">
+				<div class="call">
+					<p>SALON DILA ||<img src="images/45.png" alt="" />Call US: 111-234-56789</p>
+				</div>
+
+				<div class="clear"></div>
+			</div>
+		</div>
+		<div class="header_bottom">
+			<div class="wrap">
+				<div class="menu">
+					<ul>
+						<li class="active"><a href="index.php">Home</a></li>
+						<li><a href="login.php">Login</a></li>
+						<li><a href="Customer_Add.php">Register</a></li>
+						<li><a href="services.php">Services</a></li>
+						<li><a href="about.php">About Us</a></li>
+
+
+						<div class="clear"></div>
+					</ul>
+				</div>
+				<div class="social-icons">
+					<ul>
+						<!----------------login------>
+					</ul>
+				</div>
+				<div class="clear"></div>
+			</div>
+		</div>
+		<div class="strip"> </div>
+	</div>
+	</div>
+	<div class="main">
+		<div class="content">
+
+			<div class="wrap">
+
+				<div class="image group">
+
+					<div class="grid span_2_of_3">
+
+						<div class="contact_info">
+							<h2>Find Us Here</h2>
+							<div class="map">
+
+								<iframe width="100%" height="180" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.co.in/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;q=Kurunegala+Town+Central,+Kurunegala&amp;aq=4&amp;oq=light&amp;sll=7.4825645,80.3563574&amp;sspn=0.04941,0.104628&amp;ie=UTF8&amp;hq=&amp;hnear=Kurunegala+Town+Central,+Kurunegala&amp;t=m&amp;z=14&amp;ll=7.4825645,80.3563574&amp;output=embed"></iframe><br><small><a href="https://maps.google.co.in/maps?f=q&amp;source=embed&amp;hl=en&amp;geocode=&amp;q=Kurunegala+Town+Central,+Kurunegala&amp;aq=4&amp;oq=light&amp;sll=7.4825645,80.3563574&amp;sspn=0.04941,0.104628&amp;ie=UTF8&amp;hq=&amp;hnear=Kurunegala+Town+Central,+Kurunegala&amp;t=m&amp;z=14&amp;ll=7.4825645,80.3563574" style="color:#666;text-align:left;font-size:12px">View Larger Map</a></small>
+							</div>
+						</div>
+						<div class="company_address">
+							<h2>Company Information :</h2>
+							<p>SALON DILA,</p>
+							<p>Kurunagala Rd,</p>
+							<p>Kurunagala</p>
+							<p>Phone:(00) 222 666 444</p>
+							<p>Fax: (000) 123 45 6 78</p>
+							<p>Email: <span>slaondila@mycompany.com</span></p>
+							<p>Follow on: <span>Facebook</span>, <span>Twitter</span></p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	</div>
+	</div>
+	</div>
+	<div class="footer-strip"> </div>
+	<div class="footer">
+		<div class="wrap">
+			<div class="footer_grides">
+				<div class="footer_grid1">
+					<h3>Information</h3>
+					<ul>
+						<li><a href="#">About Us</a></li>
+						<li><a href="#">Privacy Policy</a></li>
+						<li><a href="#">Newsletter</a></li>
+						<li><a href="#">Site Map</a></li>
+					</ul>
+
+				</div>
+				<div class="footer_grid2">
+					<h3>Get In Touch</h3>
+					<div class="address">
+						<ul>
+							<li>Reflection Salon & Spa,</li>
+							<li>New City,</li>
+							<li>USA.</li>
+							<li>www.reflection@gmail.com</li>
+							<li><span>Telephone :</span> +06821458963</li>
+							<li><span>Fax :</span> +01478523690</li>
+						</ul>
+					</div>
+				</div>
+				<div class="footer_grid3">
+					<h3>Our Company</h3>
+					<div class="f_menu">
+						<ul>
+							<li><a href="#">About your Company</a></li>
+							<li><a href="#">Terms &amp; conditions</a></li>
+							<li><a href="#">News</a></li>
+							<li><a href="#">Team of professionals</a></li>
+							<li><a href="#">Testimonials</a></li>
+						</ul>
+					</div>
+				</div>
+				<div class="footer_grid4">
+					<h3>Follow US</h3>
+					<div class="img_list">
+						<ul>
+							<li><img src="images/28.png" alt=""><a href="#">Join Us on Facebook</a></li>
+							<li><img src="images/twitter.png" alt=""><a href="#">Follow Us on Twitter</a></li>
+							<li><img src="images/39.png" alt=""><a href="#">Share Us on Twitter</a></li>
+						</ul>
+					</div>
+				</div>
+				<div class="clear"></div>
+			</div>
+		</div>
+	</div>
+
+	<center>
+		<h1><b>
+				<font color="black"><i>
+						<pre></pre>
+			</b>
+	</center>
+
+</body>
+
+</html>
